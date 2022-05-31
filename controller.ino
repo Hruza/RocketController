49346fb1686ac1bf0e1dce8f4bf7f4c685570f4e
Jan Hrůza <hruza.jan1@gmail.com>
2022-05-31 21:36:56 +0200

    Added comments

--- controller.ino
+++ controller.ino
@@ -1,13 +1,11 @@
-#include <Adafruit_BMP280.h>
+#include <Adafruit_BME280.h>
 
 #include <Adafruit_MPU6050.h>
 
 #include<Servo.h>
-#define IS_DEBUG true
 
 Adafruit_MPU6050 mpu;
-Adafruit_BMP280 bmp;
-
+Adafruit_BME280 bme;
 
 Servo lock_servo;
 
@@ -32,68 +30,100 @@ enum State
 
 State state;
 
+// PARAMETERS
 #define SEALEVELPRESSURE_HPA 1013.25  //sea level pressure
-#define LIFTOFF_THRESHOLD 300 //if accleration is higher, rocket is lifted 
-#define POWER_THRESHOLD 200 //if acceleration is lower, engine burned out
+#define LIFTOFF_THRESHOLD 1000 //if accleration is higher, rocket is lifted 
+#define POWER_THRESHOLD 500 //if acceleration is lower, engine burned out
 
 #define APOGEE_PARACHUTE_DELAY 1000 //number of miliseconds to release parachute after apogee
 #define LANDING_HEIGHT 60 //if the rocket is lower that thin height from the starting location, it landed
 
+#define IS_DEBUG true // if true, serial debgging is on
 #define CONSECUTIVE_COUNTER 1 //amount of consecutive height decreases to trigger parachute
-
+                              //SHOULD BE MORE THAN 1 IF NOT TESTING
 #define SRVO_PIN 9
 #define SRVO_OPEN 255
 #define SRVO_LOCKED 0
 #define BEEPER_PIN 7
-#define READY_PIN 7
+#define READY_PIN 8
+#define OVERRIDE_PIN 6
+
+
+State prev_state;
+long beep_timer=0;
+bool beeping;
+
+// sound signal codes
+int beep_codes[] = {
+  100,5000, //IDLE
+  200,2000, //READY
+  1000,0, //POWERED_ASCENT
+  500,1000, //MECO
+  500,200, //APOGEE
+  1000,2000, //PARACHUTE_DESCEND
+  1000,500, //LANDED
+  100,100 //ERROR
+ };
+
+
 
 void setup() {
+  // initialise pins
+  pinMode(LED_BUILTIN, OUTPUT);
   pinMode(SRVO_PIN, OUTPUT);
   pinMode(BEEPER_PIN, OUTPUT);
-  pinMode(BEEPER_PIN, OUTPUT);
+  pinMode(READY_PIN, INPUT);
+  pinMode(OVERRIDE_PIN, INPUT);
 
+#if IS_DEBUG
   Serial.begin(9600);
-  
-  // put your setup code here, to run once:
+#endif
+
   state=IDLE;
 
-  if (!bmp.begin()) {  
-    Serial.println("Could not find a valid bmp280 sensor, check wiring!");
+  // connect sensors
+  if (!bme.begin()) {  
+#if IS_DEBUG
+    Serial.println("Could not find a valid bme280 sensor, check wiring!");
+#endif
     state=ERROR;
   }
 
   if (!mpu.begin()) {
+#if IS_DEBUG
     Serial.println("Failed to find MPU6050 chip");
+#endif
     state=ERROR;
-  }
-  
+  }  
   // set accelerometer range to +-16G
   mpu.setAccelerometerRange(MPU6050_RANGE_16_G);
-
   // set gyro range to +- 500 deg/s
   mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-
   // set filter bandwidth to 21 Hz
   mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
-  
-  
-  init_height = bmp.readAltitude(SEALEVELPRESSURE_HPA);
+
+  // set initial height
+  init_height = bme.readAltitude(SEALEVELPRESSURE_HPA);
   height=init_height;
 
+  // initialize and loch parachute servo
   lock_servo.attach(SRVO_PIN);
   lock_servo.write(SRVO_LOCKED);
   waitTime=0;
+#if IS_DEBUG
   if(state==IDLE){
     Serial.println("Initialised without problems");
   }
   Serial.println("x y z sqrMagnitude STATE");
+#endif
 }
 
 void loop() {
+  // read sensors
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
 
-  #if IS_DEBUG
+#if IS_DEBUG
     Serial.print(a.acceleration.x);
     Serial.print(" "); // a space ' ' or  tab '\t' character is printed between the two values.
     Serial.print(a.acceleration.y);
@@ -104,15 +134,24 @@ void loop() {
        (a.acceleration.y*a.acceleration.y) + 
        (a.acceleration.z*a.acceleration.z)));
     Serial.print(" "); // a space ' ' or  tab '\t' character is printed between the two values.
-    Serial.println(state);
-  #endif
+    Serial.print(state);
+#endif
+
+  // manual parachute override
+  if(state<4  && digitalRead(OVERRIDE_PIN)){
+    state = APOGEE;
+    waitTime=0;
+  }
+
+  // perform state check
   if(millis() > waitTime ){
   switch(state){
-    case IDLE:
-      //unlock manually?
-      state=READY;
+    case IDLE:  // rocket waits for signal
+      if(digitalRead(READY_PIN)){
+        state=READY;
+      }
     break;
-    case READY:
+    case READY: // rocket is ready for launch and waits for acceleration
        if((a.acceleration.z*a.acceleration.z)>LIFTOFF_THRESHOLD){
           counter++;
           if(counter>CONSECUTIVE_COUNTER){
@@ -125,7 +164,7 @@ void loop() {
           counter=0;
         }  
     break;
-    case POWERED_ASCENT:
+    case POWERED_ASCENT: // engine is on, waiting for accelearation to decrease
       if((a.acceleration.x*a.acceleration.x) + 
        (a.acceleration.y*a.acceleration.y) + 
        (a.acceleration.z*a.acceleration.z)<POWER_THRESHOLD){
@@ -140,8 +179,8 @@ void loop() {
           counter=0;
         }  
     break;
-    case MECO:
-        height = bmp.readAltitude(SEALEVELPRESSURE_HPA);
+    case MECO: // engine is off, altitude increases, waiting for altitude to start decreasing
+        height = bme.readAltitude(SEALEVELPRESSURE_HPA);
         if(height<prev_height){
           counter++;
           if(counter>CONSECUTIVE_COUNTER){
@@ -154,12 +193,12 @@ void loop() {
           counter=0;
         }
     break;
-    case APOGEE:
+    case APOGEE: // opening parachute
       lock_servo.write(SRVO_OPEN);
       state=PARACHUTE_DESCENT;
     break;
-    case PARACHUTE_DESCENT:
-      if (bmp.readAltitude(SEALEVELPRESSURE_HPA) <= init_height+LANDING_HEIGHT)
+    case PARACHUTE_DESCENT: // parachute is out, waiting to reach the ground
+      if (bme.readAltitude(SEALEVELPRESSURE_HPA) <= init_height+LANDING_HEIGHT)
         state=LANDED;
         waitTime=millis()+5000;
     break;
@@ -170,25 +209,15 @@ void loop() {
     break;
   }
   }
+
+  // sound signal
   beep(state);
+#if IS_DEBUG
+  Serial.println();
+#endif
 }
 
-State prev_state;
-long beep_timer=0;
-bool beeping;
-
-int beep_codes[] = {
-  200,5000, //IDLE
-  200,2000, //READY
-  1000,0, //POWERED_ASCENT
-  500,1000, //MECO
-  500,200, //APOGEE
-  1000,2000, //PARACHUTE_DESCEND
-  1000,500, //LANDED
-  200,200 //ERROR
-  };
-
-
+// sound signal for every stage of flight
 void beep(State curr_state){
     if(prev_state!=curr_state){
       beep_timer=0;
@@ -198,12 +227,18 @@ void beep(State curr_state){
     if(millis()>beep_timer)
     {
       beep_timer=millis() + beep_codes[(2*curr_state)+(beeping?1:0)];
-      beeping=!beeping;
+      beeping=beeping?false:true;
       if(beeping){
         tone(BEEPER_PIN, 500);
+        digitalWrite(LED_BUILTIN, HIGH);
       }
       else{
         noTone(BEEPER_PIN);
+        digitalWrite(LED_BUILTIN, LOW);
       }
     }
+#if IS_DEBUG
+    Serial.print(" ");
+    Serial.print(beep_timer);
+#endif
 }
